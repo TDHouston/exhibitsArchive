6fbab3c2c227c1002f2c7f90f10407655565bae4
TDHouston <trentdhouston@gmail.com>
2024-01-16 18:01:43 -0500

    basic set up configured

--- my-exarchive/src/App.jsx
+++ my-exarchive/src/App.jsx
@@ -1,25 +1,87 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+// import { Canvas, } from "@react-three/fiber";
 
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Setup() {
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    // Sizes
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      sizes.width / sizes.height,
+      1,
+      500
+    );
+    camera.position.z = 3;
+    scene.add(camera);
+
+    // Objects
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas);
+    controls.enableDamping = true;
+
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    const sceneHandler = () => {
+      // Update controls
+      controls.update();
+
+      // Render
+      renderer.render(scene, camera);
+
+      window.requestAnimationFrame(sceneHandler);
+    };
+
+    sceneHandler();
+  }, []);
+
+  return <canvas ref={canvasRef}></canvas>;
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <Setup />
+    </>
+  );
+}
+export default App;
